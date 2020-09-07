b4d7287521caeacf3e3e6bee5ae3a4033917b581
Eric Liu <ericliu314@gmail.com>
2020-09-06 20:39:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/**
+ * @class Model
+ *
+ * Manages the data of the application.
+ */
+class Model {
+  constructor() {}
+}
+
+/**
+ * @class View
+ *
+ * Visual representation of the model.
+ */
+class View {
+  constructor() {}
+}
+
+/**
+ * @class Controller
+ *
+ * Links the model and the view.
+ *
+ * @param model
+ * @param view
+ */
+class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  }
+}
+
+const app = new Controller(new Model(), new View());
