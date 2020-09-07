426a92002fe64d60d37d594908c1e536b69a4976
Eric Liu <ericliu314@gmail.com>
2020-09-07 18:52:45 -0400

    Add and remove items from table

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ class Model {
   bindOnChange(callback) {
     this.onChange = callback;
   }
+
+  get balance() {
+    return this.item.reduce((acc, curr) => (
+      acc + (curr.type === 'Income' ? curr.amount : -curr.amount
+    )));
+  }
 }
 
 /**
@@ -45,6 +51,65 @@ class View {
     const element = document.querySelector(selector);
     return element;
   }
+
+  bindAddItem(handler) {
+    this.addButton.addEventListener('click', event => {
+      if (!this.amountInput.value.length || !this.descInput.value.length) {
+        return event.preventDefault();
+      }
+
+      const item = {
+        amount: Number(this.amountInput.value),
+        desc: this.descInput.value,
+        type: this.typeInput.value,
+      };
+
+      handler(item);
+    });
+  }
+
+  bindOnDelete(callback) {
+    this.onDelete = callback;
+  }
+
+  bindAmountInputValidation(handler) {
+    this.amountInput.addEventListener('keyup', () => {
+      handler(amount);
+    });
+  }
+
+  renderTable(items) {
+    this.itemsTable.innerHTML = '';
+    for (const item of items) {
+      const row = document.createElement('tr');
+
+      const colAmount = document.createElement('td');
+      colAmount.textContent = item.amount;
+      row.appendChild(colAmount);
+
+      const colDesc = document.createElement('td');
+      colDesc.textContent = item.desc;
+      row.appendChild(colDesc);
+
+      const colType = document.createElement('td');
+      colType.textContent = item.type;
+      row.appendChild(colType);
+
+      const colDelete = document.createElement('td');
+      const deleteButton = document.createElement('a');
+      deleteButton.setAttribute('class', 'uk-icon-link');
+      deleteButton.setAttribute('uk-icon', 'trash');
+      deleteButton.addEventListener('click', () => {
+        if (this.onDelete) {
+          this.onDelete(item.id);
+        }
+      });
+      colDelete.appendChild(deleteButton);
+      row.appendChild(colDelete);
+
+      this.itemsTable.appendChild(row);
+    }
+  }
 }
 
 /**
@@ -59,7 +124,43 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+
+    this.model.bindOnChange(this.onItemsChange);
+
+    this.view.bindAddItem(this.onAddItem);
+    this.view.bindOnDelete(this.onDeleteItem);
+    this.view.bindAmountInputValidation(this.validateAmountInput);
   }
+
+  onItemsChange = items => {
+    console.log(items);
+    this.view.renderTable(items);
+  }
+
+  onAddItem = item => {
+    this.model.addItem({
+      id: generateID(),
+      ...item,
+    });
+  };
+
+  onDeleteItem = id => {
+    this.model.deleteItem(id);
+  };
+
+  validateAmountInput = amount => {
+    if (!isValidAmount(amount.value)) {
+      amount.value = amount.value.slice(0, -1);
+    }
+  };
 }
 
 const app = new Controller(new Model(), new View());
+
+function isValidAmount(num) {
+  return isFinite(num) && /^\d+((\.\d{1,2})?$)|(\.$)/.test(num);
+}
+
+function generateID() {
+  return Math.random().toString(36).substr(2, 9);
+}
